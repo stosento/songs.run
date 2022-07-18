1c8c51bd2b7e697af2add3d814afed4747c6cd57
Stephen Osentoski <stosento@umich.edu>
2022-07-17 21:09:40 -0400

    Adding server, login, web playback

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(proxy(`/auth/**`, { 
+        target: 'http://localhost:5000' 
+    }));
+};
