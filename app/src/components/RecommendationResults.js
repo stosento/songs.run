f9e9d2b3597b3dfb4c9776ec3b5401c1f68bc473
Stephen Osentoski <stosento@umich.edu>
2023-03-20 20:00:42 -0400

    Adding table library

--- app/src/components/RecommendationResults.js
+++ app/src/components/RecommendationResults.js
@@ -2,6 +2,32 @@ import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { Row, Col } from 'react-bootstrap';
 import TrackInfo from './TrackInfo';
+import DataTable from 'react-data-table-component';
+
+const columns = [
+    {
+        name: 'Title',
+        selector: row => row.title,
+    },
+    {
+        name: 'Year',
+        selector: row => row.year,
+    },
+];
+
+
+const data = [
+    {
+        id: 1,
+        title: <audio controls src={"props.track.preview"}/>,
+        year: '1988',
+    },
+    {
+        id: 2,
+        title: <audio controls src={"props.track.preview"}/>,
+        year: '1984',
+    },
+]
 
 const RecommendationResults = (props) => {
     const rows = props.recommendations.map((rec, index) => {
@@ -9,9 +35,15 @@ const RecommendationResults = (props) => {
     })
 
     return (
+        <>
         <Container>
             {rows}
         </Container>
+        <DataTable
+            columns={columns}
+            data={data}
+        />
+        </>
     );
 }
 
